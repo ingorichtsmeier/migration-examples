9519d135cc93b14ee5705d058d2b23e18e40ad50
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-07-29 12:22:03 +0200

    Example 1 runs successfully

--- migration-example-1/src/main/java/com/camunda/consulting/RejectionNotificationDelegate.java
+++ migration-example-1/src/main/java/com/camunda/consulting/RejectionNotificationDelegate.java
@@ -4,13 +4,17 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RejectionNotificationDelegate implements JavaDelegate {
 
   private static final Logger LOG = LoggerFactory.getLogger(RejectionNotificationDelegate.class);
 
   public void execute(DelegateExecution execution) throws Exception {
-    LOG.info("execute {} of process instance {}", execution.getCurrentActivityName(), execution.getProcessInstanceId());
+    LOG.info("execute {} of process instance {}", 
+        execution.getCurrentActivityName(), 
+        execution.getProcessInstanceId());
   }
 
 }
